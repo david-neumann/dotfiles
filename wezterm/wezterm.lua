c1b785ef4d4f15abe819ddad1a4977959bcc6707
David Neumann <neumann.d.david@gmail.com>
2023-09-21 10:51:26 -0500

    fix for wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,30 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+config.font = wezterm.font 'MonoLisa'
+config.font_size = 16
+
+-- For example, changing the color scheme:
+config.color_scheme = 'rose-pine-moon'
+
+-- Pane selection
+config.keys = {
+  {
+    key = '0',
+    mods = 'CTRL',
+    action = wezterm.action.PaneSelect
+  }
+}
+
+-- and finally, return the configuration to wezterm
+return config
