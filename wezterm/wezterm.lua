ea0c89a2a6cbe858859cd4406e4f384600ac0230
David Neumann <neumann.d.david@gmail.com>
2023-10-10 10:20:21 -0500

    fixes for oh-my-zsh

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,23 +7,23 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
-config.font = wezterm.font 'MonoLisa'
+config.font = wezterm.font("MonoLisa")
 config.font_size = 16
 
 -- For example, changing the color scheme:
-config.color_scheme = 'rose-pine-moon'
+config.color_scheme = "rose-pine-moon"
 
 -- Pane selection
 config.keys = {
-  {
-    key = '0',
-    mods = 'CTRL',
-    action = wezterm.action.PaneSelect
-  }
+	{
+		key = "UpArrow",
+		mods = "CTRL",
+		action = wezterm.action.PaneSelect,
+	},
 }
 
 -- and finally, return the configuration to wezterm
